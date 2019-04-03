92426359830de6567f5939ac2d5d73b37a2b1021
David <david.lecoin@yahoo.com>
2019-04-03 09:12:30 +0200

    Page F.A.Q ajoutée

---
+++ faq.php
@@ -0,0 +1,76 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <title>Jicéda - F.A.Q</title>
+  </head>
+  <body>
+  <header>
+    <nav class="text-center">
+      <h1>NAVBAR</h1>
+    </nav>
+  </header>
+
+
+    <div class="container">
+    <div class="row">
+        <div class="col-sm-12 col-md-12 col-lg-12 col-xl-12">
+            <div class="card text-center">
+              <div class="card-body">
+                <H3>Qu'est-ce que Jicéda ?</H3>
+                <br>
+                <p>Jicéda est un réseau social permettant aux personnes d'échanger leurs avis avec d'autres personnes en France.</p>
+              </div>
+            </div>
+          </div>
+      </div>
+      <br>
+      <div class="row">
+        <div class="col-sm-12 col-md-12 col-lg-12 col-xl-12">
+            <div class="card text-center">
+              <div class="card-body">
+                <H3>Que se passe-t-il en cas de conflit ?</H3>
+                <br>
+                <p>Si un conflit se génère, il sera traité au plus vite, si vous disposez de preuves, contactez nous <a href="./contact.html">ici</a></p>
+              </div>
+            </div>
+          </div>
+      </div>
+
+     
+      <br>
+      <div class="row">
+        <div class="col-sm-12 col-md-12 col-lg-12 col-xl-12">
+            <div class="card text-center">
+              <div class="card-body">
+                <H3>Votre compte est innaccessible?</H3>
+                <br>
+                <p>Vous ne parvenez pas à vous connecter? Même en retrouvant le mot de passe ? Contactez l'équipe <a href="./contact.html">ici</a></p>
+              </div>
+            </div>
+          </div>
+      </div>
+      <br>
+      <div class="row">
+        <div class="col-sm-12 col-md-12 col-lg-12 col-xl-12">
+            <div class="card text-center">
+              <div class="card-body">
+                <H3>Comment contacter l'équipe Jicéda ?</H3>
+                <br>
+                <p>Des questions ? Des bugs ? Des soucis ? Contactez nous <a href="./contact.html">ici</a> dès maintenant</p>
+              </div>
+            </div>
+          </div>
+      </div>
+    </div>
+
+
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
